f648b9b8ea0836189e557053f6423a4e253bc949
Brandon Miller <bmiller340@gmail.com>
2020-05-17 14:19:47 -0400

    Attempt at fixing folder issues also code for checking for a winner

---
+++ program.cs/tiktaktoe/Checkwin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Checkwin
+{
+	public bool Checkwin()
+	{
+
+
+
+
+		/*int size = 3;
+		bool HasWon = false;
+		for (int CheckRow = 0; CheckRow < size; CheckRow++)
+        {
+			HasWon = false;
+			if (Board[CheckRow, 0] != "");
+        }*/
+	}
+}
